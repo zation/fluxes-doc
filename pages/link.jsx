cc32f7ffb2fa9e4333f7ef8e5a893026eb8af9b6
Liu Yang <zation1@gmail.com>
2015-07-31 23:41:48 +0800

    use code mirror to show Link example

--- pages/link.jsx
+++ pages/link.jsx
@@ -1,10 +1,14 @@
 var React = require('react');
+var Playground = require('component-playground');
+
+var Link = require('fluxes/components/link.jsx');
+var LinkExample = require('../examples/link.text');
 
 module.exports = React.createClass({
   render: function() {
     return (
       <div>
-        Link
+        <Playground collapsableCode codeText={LinkExample} scope={{React: React, Link: Link}}/>
       </div>
     );
   }
