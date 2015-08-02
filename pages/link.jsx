6f74ca084fc58ec52a39577ad0b6c0c8cacda511
Liu Yang <zation1@gmail.com>
2015-08-02 15:21:28 +0800

    improve preview and editor style

--- pages/link.jsx
+++ pages/link.jsx
@@ -4,12 +4,18 @@ var Playground = require('component-playground');
 var Link = require('fluxes/components/link.jsx');
 var LinkExample = require('../examples/link.text');
 
+
 module.exports = React.createClass({
   render: function() {
     return (
       <div>
+        <h2>Link</h2>
+
+        <p>Link Component</p>
+
         <Playground codeText={LinkExample}
                     lineNumbers
+                    previewFirst
                     theme="monokai"
                     noRender={false}
                     scope={{React: React, Link: Link}}/>
