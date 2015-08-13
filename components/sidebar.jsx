6d6d00c392f9cffd446b097beb40f8c262b6cb0e
Liu Yang <zation1@gmail.com>
2015-08-13 22:00:24 +0800

    improve sidebar style

--- components/sidebar.jsx
+++ components/sidebar.jsx
@@ -1,14 +1,22 @@
 var React = require('react');
 var Link = require('fluxes/components/link.jsx');
 var _ = require('lodash');
+var Reflux = require('reflux');
+var classnames = require('classnames');
 
+var RoutesStore = require('fluxes/stores/routes');
 var SidebarRoutes = require('../constants/sidebar-routes');
 
 module.exports = React.createClass({
+  mixins: [Reflux.connectFilter(RoutesStore, 'currentRoute', function(routes) {
+    return _.last(routes);
+  })],
+
   render: function() {
+    var currentRoute = this.state.currentRoute;
     var list = _.map(SidebarRoutes, function(route, index) {
       return (
-        <li key={index}>
+        <li key={index} className={classnames({active: currentRoute === route.path})}>
           <Link href={route.path}>{route.text}</Link>
         </li>
       );
@@ -16,7 +24,7 @@ module.exports = React.createClass({
 
     return (
       <nav>
-        <ul className="nav">
+        <ul className="nav nav-pills nav-stacked">
           {list}
         </ul>
       </nav>
